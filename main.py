ebeb9a8927f20d58f799cc66033edca13286db98
Pedro Martos <54295971+P2D3R4@users.noreply.github.com>
2022-05-26 22:35:32 +0000

    dsasd

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
-app = Flask(__name__)
+app = Flask('__name__')
 
 @app.route('/')
-def index():
-    return 'Minha primeira rota!'
+def minha_primira_rota():
+  return '<h1>Minha primeira rota!</h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0')
